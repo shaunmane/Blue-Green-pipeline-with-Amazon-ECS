f63384d5ffcfe04dcd66187c196045e5a807df87
Shaun <shaunmane.sm@gmail.com>
2025-08-15 21:31:46 +0200

    just some files

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "CIDR Range for lab VPC"
+  type = string
+}
+
+variable "azs_use1" {
+  description = "Availability Zones for the region"
+  type = list()
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR ranges for the private subnets"
+  type = list()
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR ranges for the private subnets"
+  type = list()
+}
