8e05ba1bc80d99484d369517e0215367b811cecc
Shaun <shaunmane.sm@gmail.com>
2025-08-22 16:39:28 +0200

    added terraform docs formatter

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,17 @@
 terraform {
-  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.98.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "s3" {
+    bucket = "value"
+    region = "value"
+    dynamodb_table = "value"
+  }
 }
