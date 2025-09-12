166e1d606a8badcfc503a600407851ba46b8d6d7
Shaun Mane <shaunmane.sm@gmail.com>
2025-09-12 21:39:05 +0000

    added backend configs

---
+++ terraform/s3.tf
@@ -0,0 +1,21 @@
+# S3 Bucket for Terraform state
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-ecs-state-bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "terraform-backend"
+    Environment = "Dev"
+  }
+}
+
+# Enable verisoning for state history
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
