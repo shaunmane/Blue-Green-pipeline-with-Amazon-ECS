6b3e2d23ba538f7c4fe221d01cc306df99b6f8bb
Shaun <shaunmane.sm@gmail.com>
2025-10-02 18:32:14 +0200

    added log group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
-terraform {
+# Log Group where taskdef is configured to send all logs
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name = "tripmgmt-demo-ecstask-loggrp"
 
+  retention_in_days = 5
+
+  tags = {
+    Environment = "dev"
+    Application = "tripmgmt-demo"
+  }
 }
